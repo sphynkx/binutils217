b49ae4919a13234f4b4a3c9f5b4039d4098f8924
sphynkx <sfynkx@gmail.com>
2026-03-07 20:51:10 +0200

    Fix `bfd/plan9-i386.c` to generate correct a.out structure. (Caused segfault in linker).

--- bfd/plan9-i386.c
+++ bfd/plan9-i386.c
@@ -54,6 +54,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 #include "libbfd.h"
 #include "libaout.h"
 
+#include "aout/aout64.h"
+
 /* Binutils 2.17 uses bfd_boolean and TRUE/FALSE.  */
 #define boolean bfd_boolean
 #ifndef true
@@ -75,6 +77,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 
 /* Plan 9 uses static linking; ignore dynamic-link related hooks.  */
 
+/*
 static bfd_boolean
 MY (add_one_symbol) (struct bfd_link_info *info,
                      struct aout_link_hash_entry *h,
@@ -90,6 +93,8 @@ MY (add_one_symbol) (struct bfd_link_info *info,
   (void) value; (void) copy; (void) collect; (void) hashp;
   return TRUE;
 }
+*/
+
 
 static bfd_boolean
 MY (write_dynamic_symbol) (bfd *output_bfd,
@@ -125,7 +130,7 @@ static CONST struct aout_backend_data MY(backend_data) = {
 	MY_set_sizes,
 	0,	/* exec_header_not_counted */
 	0,	/* add_dynamic_symbols */
-	MY(add_one_symbol),
+	0/*MY(add_one_symbol)*/, /* add_one_symbol (use generic) */
 	0,	/* link_dynamic_object */
 	MY(write_dynamic_symbol),
 	0,	/* check_dynamic_reloc */
@@ -199,6 +204,9 @@ some_plan9_object_p (bfd *abfd,
      const bfd_target *(*callback_to_real_object_p) PARAMS ((bfd *));
 */
 {
+	obj_sym_filepos(abfd) = N_SYMOFF(*execp);
+	obj_str_filepos(abfd) = N_STROFF(*execp);
+
 	struct aout_data_struct *rawptr, *oldrawptr;
 	const bfd_target *result;
 
@@ -232,10 +240,16 @@ some_plan9_object_p (bfd *abfd,
 
 	bfd_get_start_address (abfd) = execp->a_entry;
 
+/* 
+// Olde code - caused segfault on linker
 	obj_aout_symbols (abfd) = (aout_symbol_type *)NULL;
-	bfd_get_symcount (abfd) = 0;	/* XXX */
+	bfd_get_symcount (abfd) = 0;	/ * XXX * /
 	obj_reloc_entry_size (abfd) = 1;
 	obj_symbol_entry_size (abfd) = 1;
+*/
+	bfd_get_symcount (abfd) = execp->a_syms / sizeof (struct external_nlist);
+	obj_reloc_entry_size (abfd) = RELOC_STD_SIZE;
+	obj_symbol_entry_size (abfd) = EXTERNAL_NLIST_SIZE;
 
 #ifdef USE_MMAP
 	bfd_init_window (&obj_aout_sym_window (abfd));
@@ -390,82 +404,192 @@ putsym(bfd *abfd, int type, char *prefix, char *name, bfd_vma value)
 
 static boolean
 MY(slurp_symbol_table) (bfd *abfd)
-/*
-     bfd *abfd;
-*/
 {
-	aout_symbol_type *cached;
-	size_t cached_size;
-	unsigned char *syms, *p, *ep;
-	int i, n, nsyms;
-	asection *sec;
+  aout_symbol_type *cached;
+  bfd_size_type cached_size;
+  struct external_nlist *syms;
+  bfd_size_type sym_bytes;
+  bfd_size_type sym_count;
+  char *strings;
+  bfd_size_type str_size;
+  bfd_byte lenbuf[4];
+  bfd_size_type i;
+  struct internal_exec *execp;
+  file_ptr sympos;
+  file_ptr strpos;
 
-	/* been here, done that */
-	if (obj_aout_symbols (abfd) != NULL)
-		return true;
+  /* been here, done that */
+  if (obj_aout_symbols (abfd) != NULL)
+    return true;
 
-	n = exec_hdr (abfd)->a_syms;
-	if(n == 0)
-		return true;
-	syms = bfd_malloc(n);
-	if (syms == NULL)
-		return false;
-	if (bfd_seek (abfd, obj_sym_filepos (abfd), SEEK_SET) != 0
-	|| ((int)bfd_read (syms, 1, n , abfd) != n )) {
-		free (syms);
-		return false;
-	}
-	p = syms;
-	ep = p+n;
-	nsyms = 0;
-	for(;;) {
-		p += 5;
-		while(p < ep && *p != '\0')
-			p++;
-		nsyms++;
-	}
-	bfd_get_symcount (abfd) = nsyms;
-	obj_aout_external_sym_count (abfd) = nsyms;
+  execp = exec_hdr (abfd);
+  sym_bytes = execp->a_syms;
+  if (sym_bytes == 0)
+    return true;
 
-	cached_size = (nsyms * sizeof (aout_symbol_type));
-	cached = (aout_symbol_type *) bfd_malloc (cached_size);
-	if (cached == NULL && cached_size != 0)
-		return false;
-	if (cached_size != 0)
-		memset (cached, 0, cached_size);
+  /* Sanity: must be multiple of external nlist size.  */
+  if ((sym_bytes % EXTERNAL_NLIST_SIZE) != 0)
+    {
+      bfd_set_error (bfd_error_wrong_format);
+      return false;
+    }
 
- 	p = syms;
-	for(i = 0; i < nsyms; i++) {
-		cached[i].symbol.value = bfd_h_get_32(abfd, p);
-		cached[i].symbol.name = strdup(&p[5]);
-		switch(p[4] & ~0x80) {
-		case 'T':
-		case 'L':
-			cached[i].symbol.flags = BSF_GLOBAL;
-			sec = obj_textsec(abfd);
-		case 't':
-		case 'l':
-			cached[i].symbol.flags = BSF_LOCAL;
-			sec = obj_textsec(abfd);
-		case 'D':
-			cached[i].symbol.flags = BSF_GLOBAL;
-			sec = obj_datasec(abfd);
-		case 'd':
-			cached[i].symbol.flags = BSF_LOCAL;
-			sec = obj_datasec(abfd);
-		case 'B':
-			cached[i].symbol.flags = BSF_GLOBAL;
-			sec = obj_bsssec(abfd);
-		case 'b':
-			cached[i].symbol.flags = BSF_LOCAL;
-			sec = obj_bsssec(abfd);
-		}
-		cached[i].symbol.value -= sec->vma;
-	}
+  sym_count = sym_bytes / EXTERNAL_NLIST_SIZE;
 
-	obj_aout_symbols (abfd) = cached;
-	free(syms);
-	return true;
+  sympos = obj_sym_filepos (abfd);
+  if (sympos == 0)
+    {
+      /* If backend didn't set it, fall back to a.out macro.  */
+      sympos = (file_ptr) N_SYMOFF (*execp);
+      obj_sym_filepos (abfd) = sympos;
+    }
+
+  /* String table position: usually immediately after symtab.  */
+  strpos = obj_str_filepos (abfd);
+  if (strpos == 0)
+    {
+      strpos = (file_ptr) N_STROFF (*execp);
+      obj_str_filepos (abfd) = strpos;
+    }
+
+  /* Read external nlist array.  Keep it in tdata so linker can use it.  */
+  syms = (struct external_nlist *) bfd_malloc (sym_bytes);
+  if (syms == NULL)
+    return false;
+
+  if (bfd_seek (abfd, sympos, SEEK_SET) != 0
+      || bfd_bread ((void *) syms, sym_bytes, abfd) != sym_bytes)
+    {
+      free (syms);
+      return false;
+    }
+
+  /* Read string table length (big-endian 32-bit), then strings.  */
+  if (bfd_seek (abfd, strpos, SEEK_SET) != 0
+      || bfd_bread ((void *) lenbuf, (bfd_size_type) 4, abfd) != 4)
+    {
+      free (syms);
+      return false;
+    }
+
+  str_size = bfd_getb32 (lenbuf);
+
+  /* In a.out the length includes the 4-byte length itself.
+     Accept 0/4 as empty.  */
+  if (str_size < 4)
+    {
+      /* Treat as empty strings.  */
+      str_size = 0;
+      strings = NULL;
+    }
+  else
+    {
+      bfd_size_type payload = str_size - 4;
+
+      strings = (char *) bfd_malloc (payload);
+      if (strings == NULL)
+        {
+          free (syms);
+          return false;
+        }
+
+      if (payload != 0
+          && bfd_bread ((void *) strings, payload, abfd) != payload)
+        {
+          free (strings);
+          free (syms);
+          return false;
+        }
+
+      /* For convenience, keep external_string_size as payload bytes.  */
+      str_size = payload;
+    }
+
+  /* Build cached canonical symbols.  */
+  cached_size = sym_count * sizeof (aout_symbol_type);
+  cached = (aout_symbol_type *) bfd_malloc (cached_size);
+  if (cached == NULL && cached_size != 0)
+    {
+      if (strings) free (strings);
+      free (syms);
+      return false;
+    }
+  if (cached_size != 0)
+    memset (cached, 0, cached_size);
+
+  for (i = 0; i < sym_count; i++)
+    {
+      unsigned long n_strx;
+      unsigned int type;
+      bfd_vma value;
+      asection *sec;
+
+      n_strx = bfd_getb32 (syms[i].e_strx);
+      type = syms[i].e_type[0];
+      value = bfd_getb32 (syms[i].e_value);
+
+      cached[i].symbol.the_bfd = abfd;
+      cached[i].symbol.value = value;
+
+      /* Name resolution.  */
+      if (strings == NULL || n_strx == 0)
+        {
+          cached[i].symbol.name = "";
+        }
+      else if (n_strx >= str_size)
+        {
+          /* Bad string index => wrong format.  */
+          cached[i].symbol.name = "";
+          /* You can also choose to fail hard here:
+             bfd_set_error (bfd_error_wrong_format); ... */
+        }
+      else
+        {
+          cached[i].symbol.name = strings + n_strx;
+        }
+
+      /* Section + flags.  Only handle the standard N_TYPE cases.  */
+      sec = bfd_abs_section_ptr;
+
+      switch (type & N_TYPE)
+        {
+        case N_TEXT:
+          sec = obj_textsec (abfd);
+          break;
+        case N_DATA:
+          sec = obj_datasec (abfd);
+          break;
+        case N_BSS:
+          sec = obj_bsssec (abfd);
+          break;
+        case N_UNDF:
+          sec = bfd_und_section_ptr;
+          break;
+        default:
+          /* Stabs/debug/etc: mark as debugging to keep BFD/linker sane.  */
+          cached[i].symbol.flags |= BSF_DEBUGGING;
+          sec = bfd_abs_section_ptr;
+          break;
+        }
+
+      cached[i].symbol.section = sec;
+
+      if (type & N_EXT)
+        cached[i].symbol.flags |= BSF_GLOBAL;
+      else
+        cached[i].symbol.flags |= BSF_LOCAL;
+    }
+
+  obj_aout_symbols (abfd) = cached;
+  bfd_get_symcount (abfd) = sym_count;
+
+  /* Make generic a.out code happy: store external tables too.  */
+  obj_aout_external_syms (abfd) = syms;
+  obj_aout_external_sym_count (abfd) = sym_count;
+  obj_aout_external_strings (abfd) = strings;
+  obj_aout_external_string_size (abfd) = str_size;
+
+  return true;
 }
 
 long
